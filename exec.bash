259233e253b13250855790a53c30dbbe37be8bd3
Andrey <74958459+Vlasenko2006@users.noreply.github.com>
2025-02-02 17:01:38 +0100

    Update exec.bash

--- exec.bash
+++ exec.bash
@@ -2,12 +2,12 @@
 
 # SLURM SUBMIT SCRIPT
 #SBATCH --job-name=tst
-#SBATCH --nodes=1 # unfortunately 3 is the max on strand at the moment. 
+#SBATCH --nodes=1 
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=71:59:00
-#SBATCH --account=ksm
-#SBATCH --partition=pGPU
+#SBATCH --account= #specify your account
+#SBATCH --partition= #specify your partition
 #SBATCH --error=tst_nn.out
 #SBATCH -o nn.out
 
@@ -15,8 +15,8 @@
 #SBATCH --mem=0                    # Request all memory available on all nodes
 
 
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/gpfs/home/vlasenko/miniconda3/lib/
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:#specify the path to anaconda library
 
 
 
-srun /gpfs/home/vlasenko/miniconda3/envs/gpuenv2/bin/python story_telling_nn.py #Lang5_self_long.py
+srun #specify your path to python story_telling_nn.py #Lang5_self_long.py
